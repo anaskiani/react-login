b03b5589be121b59e2a560733014e164eae910e8
Anas kiani <kianianas131@gmail.com>
2023-06-20 15:58:05 +0200

    gmail login

--- src/components/login-Page.jsx
+++ src/components/login-Page.jsx
@@ -6,11 +6,27 @@ export default function LoginPage() {
   const [password, setPassword] = useState("");
   const [usernameError, setUsernameError] = useState("");
   const [dataInput, setDataInput] = useState("");
+  const [showGmailLogin, setShowGmailLogin] = useState(false);
+  const [gmailUsername, setGmailUsername] = useState("");
+  const [gmailPassword, setGmailPassword] = useState("");
 
   const submitThis = () => {
     const data = { username: username, password: password };
     setDataInput([data]);
     console.log(data);
+  };
+
+  const handleGmailLogin = () => {
+    setShowGmailLogin(true);
+  };
+
+  const handleGmailSubmit = (e) => {
+    e.preventDefault();
+    // Perform login with Gmail logic
+    console.log("Gmail login:", gmailUsername, "password:", gmailPassword);
+    // Reset form
+    setGmailUsername("");
+    setGmailPassword("");
   };
 
   return (
@@ -23,78 +39,130 @@ export default function LoginPage() {
           <div className="flex justify-center text-2xl py-5">
             Login to your account
           </div>
-          <form onSubmit={submitThis}>
-            <div className="flex flex-col">
-              <label htmlFor="username" className="mx-5 md:mx-10 lg:mx-16 my-2">
-                Username
-              </label>
-              <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={(e) => {
-                  const enteredUsername = e.target.value;
-                  setUsername(enteredUsername);
+          {!showGmailLogin ? (
+            <form onSubmit={submitThis}>
+              <div className="flex flex-col">
+                <label
+                  htmlFor="username"
+                  className="mx-5 md:mx-10 lg:mx-16 my-2"
+                >
+                  Username
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  onChange={(e) => {
+                    const enteredUsername = e.target.value;
+                    setUsername(enteredUsername);
 
-                  // Validate the username
-                  const usernameRegex = /^[a-zA-Z0-9_]+$/; // Only allow letters, numbers, and underscore
-                  const isUsernameValid =
-                    enteredUsername.length >= 4 &&
-                    usernameRegex.test(enteredUsername);
+                    // Validate the username
+                    const usernameRegex = /^[a-zA-Z0-9_]+$/; // Only allow letters, numbers, and underscore
+                    const isUsernameValid =
+                      enteredUsername.length >= 4 &&
+                      usernameRegex.test(enteredUsername);
 
-                  if (!isUsernameValid) {
-                    setUsernameError(
-                      "Username must be at least 4 characters long and only contain letters, numbers, and underscore."
-                    );
-                  } else {
-                    setUsernameError("");
-                  }
-                }}
-                autoComplete="on"
-                name="Enter username"
-                required
-                placeholder="Enter your username"
-                className="mx-5 md:mx-10 lg:mx-16 py-2 rounded-sm px-1 mb-3 "
-              />
-              {usernameError && (
-                <div className="text-red-500 mx-5 md:mx-10 lg:mx-16">
-                  {usernameError}
+                    if (!isUsernameValid) {
+                      setUsernameError(
+                        "Username must be at least 4 characters long and only contain letters, numbers, and underscore."
+                      );
+                    } else {
+                      setUsernameError("");
+                    }
+                  }}
+                  autoComplete="on"
+                  name="Enter username"
+                  required
+                  placeholder="Enter your username"
+                  className="mx-5 md:mx-10 lg:mx-16 py-2 rounded-sm px-1 mb-3"
+                />
+                {usernameError && (
+                  <div className="text-red-500 mx-5 md:mx-10 lg:mx-16">
+                    {usernameError}
+                  </div>
+                )}
+                <label
+                  htmlFor="password"
+                  className="mx-5 md:mx-10 lg:mx-16 my-2"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  autoComplete="off"
+                  placeholder="Enter your password"
+                  name="password"
+                  required
+                  className="mx-5 md:mx-10 lg:mx-16 py-2 px-1 rounded-sm"
+                />
+                <button className="flex justify-start mx-5 md:mx-10 lg:mx-16 mt-3 text-indigo-500 font-semibold">
+                  Forgot password?
+                </button>
+                <button
+                  type="submit"
+                  className="mt-6 mx-5 md:mx-10 lg:mx-16 py-2 bg-indigo-700 text-white text-xl flex items-center justify-center"
+                >
+                  Login
+                </button>
+                <div className="flex justify-center mt-5">
+                  _________________ or _________________
                 </div>
-              )}
-              <label htmlFor="password" className="mx-5 md:mx-10 lg:mx-16 my-2">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                autoComplete="off"
-                placeholder="Enter your password"
-                name="password"
-                required
-                className="mx-5 md:mx-10 lg:mx-16 py-2 px-1 rounded-sm "
-              />
-              <button className="flex justify-start mx-5 md:mx-10 lg:mx-16 mt-3 text-indigo-500 font-semibold">
-                Forgot password?
-              </button>
-              <button
-                type="submit"
-                className="mt-6 mx-5 md:mx-10  lg:mx-16 py-2 bg-indigo-700 text-white text-xl flex items-center justify-center"
-              >
-                Login
-              </button>
-              <div className="flex justify-center mt-5">
-                _________________ or _________________
+                <button
+                  type="button"
+                  className="border-[1px] border-black flex justify-center items-center md:gap-2 text-xl py-2 mt-6 mx-5 md:mx-10 lg:mx-16"
+                  onClick={handleGmailLogin}
+                >
+                  <FcGoogle size={26} /> Login with Gmail
+                </button>
               </div>
-              <button
-                type="button"
-                className="border-[1px] border-black flex justify-center items-center md:gap-2 text-xl py-2 mt-6 mx-5 md:mx-10  lg:mx-16"
-              >
-                <FcGoogle size={26} /> Login with Gmail
-              </button>
-            </div>
-          </form>
+            </form>
+          ) : (
+            <form onSubmit={handleGmailSubmit}>
+              <div className="flex flex-col">
+                <label
+                  htmlFor="gmailUsername"
+                  className="mx-5 md:mx-10 lg:mx-16 my-2"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="gmailUsername"
+                  value={gmailUsername}
+                  onChange={(e) => setGmailUsername(e.target.value)}
+                  autoComplete="on"
+                  required
+                  placeholder="Enter your email"
+                  className="mx-5 md:mx-10 lg:mx-16 py-2 px-1 rounded-sm mb-3"
+                />
+                <label
+                  htmlFor="gmailPassword"
+                  className="mx-5 md:mx-10 lg:mx-16 my-2"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="gmailPassword"
+                  value={gmailPassword}
+                  onChange={(e) => setGmailPassword(e.target.value)}
+                  autoComplete="off"
+                  placeholder="Enter your password"
+                  required
+                  className="mx-5 md:mx-10 lg:mx-16 py-2 px-1 rounded-sm"
+                />
+                <button
+                  type="submit"
+                  className="mt-6 mx-5 md:mx-10  lg:mx-16 py-2 bg-indigo-700 text-white text-xl flex items-center justify-center"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          )}
         </div>
         <div></div>
       </div>
